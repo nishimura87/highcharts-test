0f9c02ba09a35cb57a7104be9dd3d3326b5c1b37
nishimura87 <nishimura.k87@gmail.com>
2023-05-29 17:59:44 +0900

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="container">
+    <h1>{{ title }}</h1>
+    <p>{{ description }}</p>
+    <NuxtLink to="/sample">otherへ</NuxtLink>
+  </div>
+</template>
+
+<script>
+export default {
+data() {
+return {
+title: 'Nuxt.js',
+description: 'let`s learning Nuxt.js',
+}
+},
+}
+</script>
+
+<style>
+.container {
+padding: 30px;
+width:20%;
+}
+</style>
